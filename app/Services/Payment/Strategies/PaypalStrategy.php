2dbbd1d0c8453fcb5f03a9e8fbdd518d45a741f0
David Joos <drdavejoos@gmail.com>
2022-12-06 08:33:33 +0100

    Fix PayPal Strategy #2

--- app/Services/Payment/Strategies/PaypalStrategy.php
+++ app/Services/Payment/Strategies/PaypalStrategy.php
@@ -18,9 +18,11 @@ class PaypalStrategy implements PaymentStrategy
         $paypalData = $payload['purchase_units'][0];
         $shipping = $paypalData['shipping'];
 
+        ray($shipping);
+
         // Address
         $customer['address_line_1'] = $shipping['address']['address_line_1'];
-        if (!($city = $shipping['address']['admin_area_1'])) {
+        if (!($city = $shipping['address']['admin_area_1'] ?? null)) {
             $city = $shipping['address']['admin_area_2'] ?? 'Keine Angabe';
         }
         $customer['city'] = $city;
