335db8362dbb90a65e6f6da72ed65f208eae380c
David Joos <drdavejoos@gmail.com>
2023-07-20 16:37:35 +0200

    Fix upsells

--- app/Http/Controllers/Checkout/ShowController.php
+++ app/Http/Controllers/Checkout/ShowController.php
@@ -2,13 +2,10 @@
 
 namespace App\Http\Controllers\Checkout;
 
-use App\Enums\Category;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\Product;
-use App\Models\Upsell;
 use App\Notifications\OrderPaid;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -21,25 +18,28 @@ class ShowController extends Controller
      */
     public function index(Order $order): Response
     {
-
         $order->load('products', 'certificate');
 
         $order->update([
-            'status' => 'finalized'
+            'status' => 'finalized',
         ]);
 
         return Inertia::render('Checkout/Index', [
             'order' => $order,
             'addedUpsells' => $order->upsells,
-            'upsells' => Product::whereNot('type', 'certificate')->get(),
+            'upsells' => Product::whereNot('type', 'certificate')->whereDoesntHave(
+                'orders',
+                function ($query) use ($order) {
+                    $query->where('order_id', $order->id);
+                }
+            )->get(),
         ]);
     }
 
     public function thankyou(Order $order): Response
     {
-
         $order->update([
-            'status' => 'open'
+            'status' => 'open',
         ]);
 
         $order->customer->notify(new OrderPaid($order, $order->customer->name));
@@ -49,7 +49,7 @@ class ShowController extends Controller
                 'order.show',
                 now()->addMinutes(30),
                 ['order' => $order->slug]
-            )
+            ),
         ]);
     }
 }
