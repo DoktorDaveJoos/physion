5d6d81db00d87e6f07b1f2999a9c0f6156d34209
David Joos <drdavejoos@gmail.com>
2024-03-06 10:22:25 +0100

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,20 +18,20 @@ class DatabaseSeeder extends Seeder
             'first_name' => 'David',
             'last_name' => 'Joos',
             'email' => 'david@bauzertifikate.de',
-            'password' => bcrypt('testtest'),
+            'password' => bcrypt('test'),
         ]);
 
         User::factory()->create([
             'first_name' => 'Hannes',
             'last_name' => 'Jungert',
             'email' => 'hannes@bauzertifikate.de',
-            'password' => bcrypt('testtest'),
+            'password' => bcrypt('test'),
         ]);
 
-        $this->call([
-            ProductSeeder::class,
-            ResourcesSeeder::class,
-            TeamSeeder::class,
-        ]);
+//        $this->call([
+//            ProductSeeder::class,
+//            ResourcesSeeder::class,
+//            TeamSeeder::class,
+//        ]);
     }
 }
